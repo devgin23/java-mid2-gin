edeedfe4ebbba5aea52fe0eaf90ef60cedaaea09
Gin <76995293+devgin23@users.noreply.github.com>
2024-08-07 23:56:09 +0900

    유닛프린터 테스트

--- src/generic/test/ex3/UnitPrinterTest.java
+++ src/generic/test/ex3/UnitPrinterTest.java
@@ -15,7 +15,8 @@ public class UnitPrinterTest {
         shuttle3.in(new Zealot("질럿", 100));
 
         UnitPrinter.printV1(shuttle1);
-        UnitPrinter.printV2(shuttle1);
+        UnitPrinter.printV2(shuttle2);
+        UnitPrinter.printV2(shuttle3);
 
 
     }
